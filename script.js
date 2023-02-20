910b3c49e9bece117c61485dba7b5ee19c0846cf
ATsintsadze <a.tsintsadze@live.com>
2023-02-20 19:56:12 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,121 @@
+'use strict';
+
+// Element Selecting
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// rules
+const overlay = document.querySelector('.overlay');
+const openRules = document.querySelector('.btn--rules');
+const rules = document.querySelector('.rulessection');
+
+openRules.addEventListener('click', function () {
+  rules.classList.remove('hiderules');
+  overlay.classList.remove('hiderules');
+});
+
+overlay.addEventListener('click', function () {
+  overlay.classList.add('hiderules');
+  rules.classList.add('hiderules');
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    if (!openRules.classList.contains('hiderules')) {
+      overlay.classList.add('hiderules');
+      rules.classList.add('hiderules');
+    }
+  }
+});
+
+// start condition
+
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
+
+btnNew.addEventListener('click', init);
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+// dice roll function
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // 1. generating random diceroll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // 2.display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. check for rolled 1: if true
+    if (dice !== 1) {
+      // dice current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      //switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // add current score to active player score
+    scores[activePlayer] += currentScore;
+    console.log(scores[activePlayer]);
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    //  scores[1] = scores[1] + currentScore;
+
+    // check if player has 100 points
+
+    if (scores[activePlayer] >= 100) {
+      // finish game
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // change to next player
+      switchPlayer();
+    }
+  }
+});
